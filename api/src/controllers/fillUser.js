49ef300234d16ae7f61c893086fceb8e14bf021b
Kevin2003123 <kevinreyes005@gmail.com>
2023-05-16 16:17:54 -0400

    15  more Users added

--- api/src/controllers/fillUser.js
+++ api/src/controllers/fillUser.js
@@ -3,6 +3,21 @@ const fillUser = async (User) => {
     await User.bulkCreate([
       { name: "kevin2003123", password: "12345", isAdmin: true },
       { name: "franklyn", password: "12345", isAdmin: false },
+      { name: "John", password: "12345", isAdmin: false },
+      { name: "Michael", password: "12345", isAdmin: false },
+      { name: "Emily", password: "12345", isAdmin: false },
+      { name: "Sophia", password: "12345", isAdmin: false },
+      { name: "Daniel", password: "12345", isAdmin: false },
+      { name: "Olivia", password: "12345", isAdmin: false },
+      { name: "Matthew", password: "12345", isAdmin: false },
+      { name: "Emma", password: "12345", isAdmin: false },
+      { name: "Andrew", password: "12345", isAdmin: false },
+      { name: "Jessica", password: "12345", isAdmin: false },
+      { name: "David", password: "12345", isAdmin: false },
+      { name: "Sarah", password: "12345", isAdmin: false },
+      { name: "Benjamin", password: "12345", isAdmin: false },
+      { name: "Lily", password: "12345", isAdmin: false },
+      { name: "William", password: "12345", isAdmin: false },
     ]);
   } catch (error) {
     console.log(error.message);
