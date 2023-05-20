14a4b048de045bd4894c58fd195ed94b7935fa75
francogb98 <francobaudino98@gmail.com>
2023-05-20 15:24:59 -0300

    agregado de paginado

--- client/henrybuster/src/redux/actions.js
+++ client/henrybuster/src/redux/actions.js
@@ -8,7 +8,9 @@ import {
   FILTER_BY_GENRES,
   GET_RANKING,
   FILTER_RANKING,
-  GET_LANGUAGES
+  GET_LANGUAGES,
+  GET_MOVIES_ADMIN,
+  SPLIT_RECIPES_ADMIN,
 } from "./action-type";
 
 import axios from "axios";
@@ -60,11 +62,35 @@ export const getMovies = () => {
     }
   };
 };
-
 export function splitRecipes(inicio, fin) {
   return async (dispatch) => {
     dispatch({
       type: SPLIT_RECIPES,
+      inicio,
+      fin,
+    });
+  };
+}
+export const getMoviesAdmin = () => {
+  return async function (dispatch) {
+    try {
+      let movie = await axios.get(`http://localhost:3001/movies/admin`);
+      let payload = movie.data;
+      console.log(payload);
+
+      return dispatch({
+        type: GET_MOVIES_ADMIN,
+        payload: payload,
+      });
+    } catch (error) {
+      return { error: error };
+    }
+  };
+};
+export function splitRecipesAdmin(inicio, fin) {
+  return async (dispatch) => {
+    dispatch({
+      type: SPLIT_RECIPES_ADMIN,
       inicio,
       fin,
     });
@@ -131,7 +157,6 @@ export function getLanguages() {
     });
   };
 }
-
 
 export function filterByGenres(filtro) {
   return {
