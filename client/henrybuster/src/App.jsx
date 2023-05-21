9aa8bbf337a2b22a2c9f3378e7be138aa38211db
francogb98 <francobaudino98@gmail.com>
2023-05-20 23:40:42 -0300

    arregaldo

--- client/henrybuster/src/App.jsx
+++ client/henrybuster/src/App.jsx
@@ -14,25 +14,22 @@ import Admin from "./components/Admin/Admin";
 import { CreateGenre } from "./components/CreateGenre/CreateGenre";
 import MoviesAdmin from "./components/Admin/MoviesAdmin/MoviesAdmin";
 function App() {
-
   return (
     <>
-    <CartProvider>
+      <CartProvider>
+        {location.pathname === "/" && <Nav />}
 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<Detail />} />
+          <Route path="/cart" element={<Cart />} />
 
-      {location.pathname === "/" ? <Landing /> : <Nav />}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/cart" element={<Cart />} />
-
-        <Route path="/create" element={<CreateMovie />} />
-        <Route path="/update/:id" element={<UpdateMovie />} />
-        <Route path="/create/genre" element={<CreateGenre />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/movies" element={<MoviesAdmin />} /> 
-      </Routes>
+          <Route path="/create" element={<CreateMovie />} />
+          <Route path="/update/:id" element={<UpdateMovie />} />
+          <Route path="/create/genre" element={<CreateGenre />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/movies" element={<MoviesAdmin />} />
+        </Routes>
       </CartProvider>
     </>
   );
