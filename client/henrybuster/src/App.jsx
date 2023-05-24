72e404084a249d20bd3c659a32c9919c0b31acc6
Miguel Castillo <macastilloperez@yahoo.com.mx>
2023-05-23 23:13:01 -0600

    rutas agregadas

--- client/henrybuster/src/App.jsx
+++ client/henrybuster/src/App.jsx
@@ -25,27 +25,26 @@ import UpdateGenre from "./components/Admin/genres/UpdateGenre";
 function App() {
   return (
     <>
-
-
       <CartProvider>
-        <AuthProvider>      
-        <Nav />
+        <AuthProvider>
+          <Nav />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<Detail />} />
-          <Route path="/cart" element={<Cart />} />
-
-          <Route path="admin/create" element={<CreateMovie />} />
-          <Route path="admin/update/:id" element={<UpdateMovie />} />
-          <Route path="admin/create/genre" element={<CreateGenre />} />
-          <Route path="admin/update/genre/:id" element={<UpdateGenre />} />
-          <Route path="admin/AllGenre" element={<AllGenres />} />
-          <Route path="/admin/*" element={<Admin />} />
-          <Route path="/admin/movies" element={<MoviesAdmin />} />
-          <Route path="/results" element={<SearchResult />} />
-        </Routes>
- </AuthProvider>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/movie/:id' element={<Detail />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='admin/create' element={<CreateMovie />} />
+            <Route path='admin/update/:id' element={<UpdateMovie />} />
+            <Route path='admin/create/genre' element={<CreateGenre />} />
+            <Route path='admin/update/genre/:id' element={<UpdateGenre />} />
+            <Route path='admin/AllGenre' element={<AllGenres />} />
+            <Route path='/admin/*' element={<Admin />} />
+            <Route path='/admin/movies' element={<MoviesAdmin />} />
+            <Route path='/results' element={<SearchResult />} />
+          </Routes>
+        </AuthProvider>
       </CartProvider>
     </>
   );
