61e7449380c0e8bd9faf7e0fabc788ef45008d7d
erubio07 <erubio07@gmail.com>
2023-05-25 16:17:58 -0300

    deploy db

--- api/src/db.js
+++ api/src/db.js
@@ -10,15 +10,17 @@ const movie = require("./models/Movie");
 const purchase = require("./models/Purchase");
 const rating = require("./models/Rating");
 const language = require("./models/Language");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/henrybuster`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      requiere: true,
+    },
+  },
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
