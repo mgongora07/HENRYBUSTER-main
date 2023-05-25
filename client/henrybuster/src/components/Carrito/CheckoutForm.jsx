46def9416bfa6ea9cfaaf2d68e8f7ce7fb310d65
Kevin2003123 <kevinreyes005@gmail.com>
2023-05-25 18:57:54 -0400

    cambios

--- client/henrybuster/src/components/Carrito/CheckoutForm.jsx
+++ client/henrybuster/src/components/Carrito/CheckoutForm.jsx
@@ -6,7 +6,7 @@ import style from '../Styles/CheckoutForm.module.css';
 
 const CheckoutForm = () => {
   const { cartItems } = useContext(CartContext);
-  const amountDolars = cartItems.reduce((previous, current) => previous + current.amount * current.price, 0).toFixed(2);;
+  const amountDolars = cartItems.reduce((previous, current) => previous + current.amount * current.price, 0).toFixed(2);
   const stripe = useStripe();
   const elements = useElements();
   const amountInCents = Math.round(amountDolars * 100);
@@ -74,8 +74,8 @@ const CheckoutForm = () => {
         </div>
         <button className={style.boton} disabled={!stripe}>
           {loading ? (
-            <div class="spinner-border text-dark" role="status">
-              <span class="sr-only">Loading...</span>
+            <div className="spinner-border text-dark" role="status">
+              <span className="sr-only">Loading...</span>
             </div>
           ) : `Submit Payment ${amountDolars}`}
         </button>
