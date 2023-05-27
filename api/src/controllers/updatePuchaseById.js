6bca6858bea3945946fd831118671fc2495cd65c
Kevin2003123 <kevinreyes005@gmail.com>
2023-05-27 18:30:41 -0400

    cancelacionPelicula

--- api/src/controllers/updatePuchaseById.js
+++ api/src/controllers/updatePuchaseById.js
@@ -1,4 +1,4 @@
-const { Purchase } = require("../db");
+const { Purchase, PurchaseMovie, Inventory } = require("../db");
 
 const updatePuchaseById = async (req, res) => {
   const { id } = req.params;
@@ -8,8 +8,33 @@ const updatePuchaseById = async (req, res) => {
 
     const purchase = await Purchase.findByPk(id);
 
+    if(purchase.status==="cancelled"){
+        throw new Error(
+            "no se puede cambiar el estado de una devolucion"
+          );
+    }
+
     await purchase.update({status});
 
+    if(status==="cancelled"){
+       const purchaseMovie = await PurchaseMovie.findAll({where: {
+        PurchaseId: id
+        
+       }})
+
+      
+
+       for (const purchase of purchaseMovie) {
+        const inventory = await Inventory.findOne({
+          where: { MovieId: purchase.MovieId },
+        });
+        inventory.quantity += purchase.quantity;
+        await inventory.save();
+      }
+  
+
+    }
+  
     res.status(200).json(purchase);
   } catch (error) {
     res.status(500).json({ error: error.message });
