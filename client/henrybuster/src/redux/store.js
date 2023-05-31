bb238213b0106600addd3de0aa21e6db39a01220
Miguel Castillo <macastilloperez@yahoo.com.mx>
2023-05-31 08:27:34 -0600

    composeEnhancers

--- client/henrybuster/src/redux/store.js
+++ client/henrybuster/src/redux/store.js
@@ -1,13 +1,21 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducer';
 
-const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+
+
+//const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? (null || compose) : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+
+
+
 const enhancer = composeEnhancers(
         applyMiddleware(thunk),
         // other store enhancers if any
       );
 
-export const store = createStore(rootReducer, enhancer); // Corrección en esta línea
+export const store = createStore(rootReducer, enhancer);
 
